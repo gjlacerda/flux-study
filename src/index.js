87d281f04fdaf88dfc63efe8d9189832884c96de
gjlacerda <guilherme.lcd@gmail.com>
2017-08-16 22:00:49 -0300

    Used Provider from react-redux instead creating a class

--- src/index.js
+++ src/index.js
@@ -3,24 +3,7 @@ import ReactDOM from 'react-dom';
 import TodoApp from 'todoApp';
 import reducer from './reducer';
 import {createStore} from 'redux';
-import PropTypes from 'prop-types';
-
-class Provider extends React.Component {
-
-    getChildContext() {
-        return {
-            store: this.props.store
-        };
-    }
-
-    render() {
-        return this.props.children;
-    }
-}
-
-Provider.childContextTypes = {
-    store: PropTypes.object
-};
+import {Provider} from 'react-redux';
 
 ReactDOM.render(
     <Provider store={createStore(reducer)}>
